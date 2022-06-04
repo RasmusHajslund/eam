699076b550927160e296c2745a12411be2095bfc
Rasmus Aarøe Hajslund <rha@netcompany.com>
2022-06-05 00:39:03 +0200

    added models, Draft for Appliation and Components

---
+++ EnterpriseArchitectManagement/Models/Application.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EnterpriseArchitectManagement.Models
+{
+    public class Application
+    {
+        public int ID { get; set; }
+        public string Name { get; set; } = string.Empty;
+        [DataType(DataType.Date)]
+        public DateTime GoLiveDate { get; set; } = DateTime.Now;
+        [DataType(DataType.Date)]
+        public DateTime EndOfLifeDate { get; set; } = DateTime.Now.AddYears(1);
+        public string Description { get; set; } = string.Empty;
+        public enum Status
+        {
+            Planning = 1,
+            Analysis = 2,
+            Design = 3,
+            Development = 4,
+            Testing = 5,
+            Integration = 6,
+            Maintenance = 7,
+            Disposition = 8,
+            Disabled = 9,
+        }
+        public List<Application> Components { get; set; } = new List<Application>();
+    }
+}
